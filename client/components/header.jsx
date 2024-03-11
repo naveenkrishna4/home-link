82c7922567d357e5c8ffea97c5c352b033b6fe20
naveenkrishna4 <krishnapmnaveen@gmail.com>
2024-03-11 22:32:43 +0530

    feat : homelink

---
+++ client/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="bg-red-400 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3 text-red-400">
+        header
+      </div>
+    </div>
+  );
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/home";
+import Header from "../components/header";
+import SignIn from "../pages/signin";
+import SignUp from "../pages/signup";
+import About from "../pages/about";
+import Profile from "../pages/profile";
 
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
