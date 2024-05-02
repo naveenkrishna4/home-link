c045219dacacff44a4878e4b9fe66ec0251264f8
naveenkrishna4 <krishnapmnaveen@gmail.com>
2024-05-02 14:21:14 +0530

    feat: completed signin page functionality

--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -1,17 +1,49 @@
 import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
-    <div>
-      <h1 className="text-center text-3xl font-bold mt-5 my-7">Sign Up</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center text-3xl font-bold mt-5 my-7">Sign In</h1>
       <form className="flex flex-col gap-4">
-        <input
-          type="text"
-          placeholder="username"
-          id="username"
-          className="flex items-center round-lg border p-3"
-          onChange={handleChange}
-        ></input>
         <input
           type="email"
           placeholder="email"
@@ -31,9 +63,16 @@ export default function SignIn() {
           className="text-white text-center p-3 mt-3 bg-slate-700 rounded-lg hover:opacity-95 disabled:opacity-80"
           onClick={handleSubmit}
         >
-          {loading ? "LOADING..." : "SIGN UP"}
+          {loading ? "LOADING..." : "SIGN IN"}
         </button>
       </form>
+      <div className="flex mt-2 gap-2">
+        <p>Dont have an account?</p>
+        <Link to="/signup">
+          <span className="text-blue-700 ">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
