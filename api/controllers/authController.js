2bf55d8f323d31bf870e93ac4dadd16e8cb6ce6c
naveenkrishna4 <krishnapmnaveen@gmail.com>
2024-05-02 14:05:44 +0530

    feat: created signin api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../model/userModel.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +13,25 @@ export const signup = async (req, res, next) => {
       password: hashedpw,
     });
     res.status(201).json("user created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const valid = await bcrypt.compareSync(password, validUser.password);
+    if (!valid) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+    res.cookie("accessToken", token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -1,5 +1,39 @@
 import React from "react";
 
 export default function SignIn() {
-  return <div>signin</div>;
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold mt-5 my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="flex items-center round-lg border p-3"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="flex items-center round-lg border p-3"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="flex items-center round-lg border p-3"
+          onChange={handleChange}
+        ></input>
+        <button
+          disabled={loading}
+          className="text-white text-center p-3 mt-3 bg-slate-700 rounded-lg hover:opacity-95 disabled:opacity-80"
+          onClick={handleSubmit}
+        >
+          {loading ? "LOADING..." : "SIGN UP"}
+        </button>
+      </form>
+    </div>
+  );
 }
