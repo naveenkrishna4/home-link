205552c4bd14dc957590205fca40e8ec528cb4f0
naveenkrishna4 <krishnapmnaveen@gmail.com>
2024-05-26 12:09:09 +0530

    feat: completed about page

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function About() {
-  return <div> About</div>;
+  return (
+    <div className=" py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About HomeLink</h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to HomeLink, your ultimate destination for all your real estate
+        needs. Whether you're looking to buy, sell, or rent, HomeLink is
+        designed to make your real estate journey seamless, efficient, and
+        enjoyable.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At HomeLink, our mission is to revolutionize the real estate experience
+        by leveraging cutting-edge technology and exceptional customer service.
+        We aim to connect people with their dream homes while providing a
+        platform that simplifies the complex process of real estate
+        transactions.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Explore a vast database of properties, use advanced search filters,
+        experience immersive virtual tours, and benefit from expert advice.
+        HomeLink ensures secure transactions and offers a user-friendly
+        interface for everyone, from first-time homebuyers to seasoned
+        investors. Join the HomeLink community today and experience the future
+        of real estate.
+      </p>
+    </div>
+  );
 }
