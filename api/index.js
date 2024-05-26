058f5060ba8ca012abeb4c9c80f1776910dfc095
naveenkrishna4 <krishnapmnaveen@gmail.com>
2024-05-26 12:18:05 +0530

    feat: added pre installation

--- api/index.js
+++ api/index.js
@@ -3,12 +3,16 @@ import authRouter from "../api/routes/authRouter.js";
 import userRouter from "../api/routes/userRouter.js";
 import listingRouter from "../api/routes/listingRouter.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 import connectdb from "./config/dbConnect.js";
 connectdb();
+
+const __dirname = path.resolve();
+
 const app = express();
 
 app.listen(3000, () => {
@@ -21,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
